4cf3fe002627cb9483b75cfa5516bd499905f7d3
JustMonika24769 <114277414+JustMonika24769@users.noreply.github.com>
2024-03-05 07:31:02 +0800

    更新 config.js

--- config.js
+++ config.js
@@ -3,7 +3,7 @@ module.exports = {
     email: {
         host: 'smtp.qq.com', // 邮箱服务器
         user: 'XXXXXX@qq.com', // 发送邮件的邮箱地址
-        pass: 'your_token', // 发送邮件的邮箱密码
+        pass: 'your_token', // 发送邮件的邮箱密码/授权码，需要在邮箱服务商开启SMTP服务后获取授权码
         to: 'XXXXXXX@example.com' // 接收邮件的邮箱地址，可以和发送邮件的邮箱地址相同
     },
     web: {
